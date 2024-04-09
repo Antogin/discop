306d7cc5bd3b7676e7f98c50d3ad3be5f64a485e
Antogin <georges@getjust.eu>
2024-04-09 20:01:26 +0200

    save

--- src/pages/api/discogs.js
+++ src/pages/api/discogs.js
@@ -13,6 +13,7 @@ const client = twilio(accountSid, authToken);
 const from = process.env.TWILIO_WHATSAPP_FROM;
 const to = process.env.TWILIO_WHATSAPP_TO; 
 
+console.log('xxxx', process.env);
 async function sendWhatsAppMessage(messageTxt) {
   try {
     const message = await client.messages.create({
@@ -111,7 +112,7 @@ export default async function handler(req, res) {
     return res.status(405).end(); // Method Not Allowed
   }
 
-  console.log('>>>>>>>>>>>');
+  console.log('ENV ', process.env);
 
 
   try {
